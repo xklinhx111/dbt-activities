516849be4f434aaf17c450883d02e8f2b8f5d5db
Soheil Salmani <salmani.soheil@gmail.com>
2025-09-28 22:12:30 +0200

    Exercise 18

---
+++ dbt_tutorial/models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,14 @@
+with
+    source as (select * from {{ source('jaffle_shop', 'customers') }}),
+
+    renamed as (
+
+        select
+            id as customer_id,
+            name as customer_name
+
+        from source
+
+    )
+
+select * from renamed
